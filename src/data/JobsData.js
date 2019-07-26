9782f3e919d9e23001323115db20423f3dcff6b1
Vishal Patel <vishal@cabanalabs.com>
2019-07-25 17:51:35 -0700

    cleanup

--- src/data/JobsData.js
+++ src/data/JobsData.js
@@ -14,7 +14,7 @@ class JobsData {
   }
 
   loadJobSourcesFor(jobSourceName, toggledOn) {
-    return new Promise((resolve, reject) => {      
+    return new Promise((resolve, reject) => {
       this.updateStatus(`Loading data for ${jobSourceName}...`);
       GetJSON.getCompressedJSON(`../data/${jobSourceName}.json`)
       .then((jsonData) => this.processJobSourceJSON(jsonData, toggledOn))
